ca10b0c29c09b9d96e40ac09a5b39a464668fb44
EKibet <kibetedgar@gmail.com>
2018-10-09 09:13:33 +0300

    updates urls

--- igclone/urls.py
+++ igclone/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from user import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('',include('instagram.urls')),
+    url(r'^instagram/',include('instagram.urls')),
+    url(r'^$',user_views.register,name='register'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'profile/$', user_views.profile, name='profile'),
+    url(r'^friendship/', include('friendship.urls')),
+
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
